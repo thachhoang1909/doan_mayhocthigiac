c46649cf4650fb400d73eec7c9ca030c7aef8d81
Trần Quốc Long <tranquoclong.t1@gmail.com>
2018-01-19 23:29:22 +0700

    longtq - update
    longtq -update - split testtrain with a defined number of set inneed

--- split_test_train.py
+++ split_test_train.py
@@ -1,4 +1,4 @@
-import glob
+import glob, os
 from sklearn.model_selection import train_test_split
 
 """
@@ -9,10 +9,10 @@ with open('features.pkl', 'rb') as feature_file:
 features = features.reshape((features.shape[0], features.shape[2]))
 """
 
-# load image name
-images = glob.glob('images//*//*.png')
+# load all image name
+images = glob.glob('caltech-4//*//*.jpg')
 images.sort()
-
+print(len(images))
 
 # set labels for each image
 targets = []
@@ -21,27 +21,34 @@ for image in images:
 
 
 # split to train/test set
-X_train, X_test, Y_train, Y_test = train_test_split(images, targets, test_size=0.2)
 
-with open('db/db1/train.txt', mode='w') as train_file:
-    for file in X_train:
-        train_file.write(file)
-        train_file.write('\n')
 
-with open('db/db1/lbtrain.txt', mode='w') as label_train_file:
-    for file in Y_train:
-        label_train_file.write(file)
-        label_train_file.write('\n')
+num_db = 3
+for i in range(num_db):
+    X_train, X_test, Y_train, Y_test = train_test_split(images, targets, test_size=0.2)
+    db_path = 'db/db%s'%(i+1)
+    if not os.path.exists(db_path):
+        os.makedirs(db_path)
 
-with open('db/db1/test.txt', mode='w') as test_file:
-    for file in X_test:
-        test_file.write(file)
-        test_file.write('\n')
+    with open(db_path+'/train.txt', mode='w') as train_file:
+        for file in X_train:
+            train_file.write(file)
+            train_file.write('\n')
 
-with open('db/db1/lbtest.txt', mode='w') as label_test_file:
-    for file in Y_test:
-        label_test_file.write(file)
-        label_test_file.write('\n')
+    with open(db_path+'/lbtrain.txt', mode='w') as label_train_file:
+        for label in Y_train:
+            label_train_file.write(label)
+            label_train_file.write('\n')
+
+    with open(db_path+'/test.txt', mode='w') as test_file:
+        for file in X_test:
+            test_file.write(file)
+            test_file.write('\n')
+
+    with open(db_path+'/lbtest.txt', mode='w') as label_test_file:
+        for label in Y_test:
+            label_test_file.write(label)
+            label_test_file.write('\n')
 
 print("Done")
 
